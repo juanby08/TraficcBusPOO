a23c5e2a8f3ab991380c59a6861a4a3c928fb08f
Anye-code <anyelacabrera789@gmail.com>
2026-06-01 21:26:59 -0500

    feat: update MGame.h

---
+++ MGame.cpp
@@ -0,0 +1,11 @@
+#include "MGame.h"
+//#include "Game.h"
+#include <iostream>
+using namespace std;
+
+// Función principal
+int main() 
+{
+    
+    return 0;
+};
--- MGame.h
+++ MGame.h
@@ -1,2 +1,20 @@
-class MGame {
+#ifndef MGAME_H
+#define MGAME_H
+
+#include <string>
+#include <utility>
+#include "globaldefinitions.h"
+using namespace std;
+
+class MGame{
+    protected: 
+        int level;
+        bool finished;
+        int busQuantity;
+        int passengersQuantity;
+    
+    public:
+        void loadLevel();
+
 };
+#endif // MGAME_H

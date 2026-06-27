4ebe02015511145f133e3416fa2bf91c70a564c4
Carlos <carlosanibal50@gmail.com>
2026-06-27 17:58:07 -0500

    Updated Comments for all classes. Deleted not used clases.

--- GridTests.cpp
+++ GridTests.cpp
@@ -1,3 +1,13 @@
+// Curso: Fundamentos de Programacion Orientada a Objetos
+// Archivo: GridTests.cpp
+// Autores:
+// - Juan Ricardo Riaños Horta | Código: 2537573 | juan.rianos@correounivalle.edu.co
+// - Carlos Anibal Ceron del Castillo | Código: 2537097 | carlos.ilich.ceron@correounivalle.edu.co
+// - Anyela Lineth Cabrera Ordoñez | Código: 2540031 | anyela.cabrera@correounivalle.edu.co
+// - Camilo Espinal León | Código: 2538740 | camilo.espinal@correounivalle.edu.co
+// - Juan José Peña Garcés | Código: 2538880 | juan.jose.pena@correounivalle.edu.co
+// Fecha: 27/06/2026
+
 #include <iostream>
 #include <cassert>
 #include <vector>
--- testsPassengers.cpp
+++ testsPassengers.cpp
@@ -1,3 +1,13 @@
+// Curso: Fundamentos de Programacion Orientada a Objetos
+// Archivo: testsPassengers.cpp
+// Autores:
+// - Juan Ricardo Riaños Horta | Código: 2537573 | juan.rianos@correounivalle.edu.co
+// - Carlos Anibal Ceron del Castillo | Código: 2537097 | carlos.ilich.ceron@correounivalle.edu.co
+// - Anyela Lineth Cabrera Ordoñez | Código: 2540031 | anyela.cabrera@correounivalle.edu.co
+// - Camilo Espinal León | Código: 2538740 | camilo.espinal@correounivalle.edu.co
+// - Juan José Peña Garcés | Código: 2538880 | juan.jose.pena@correounivalle.edu.co
+// Fecha: 27/06/2026
+
 #include <cassert>
 #include <iostream>
 
